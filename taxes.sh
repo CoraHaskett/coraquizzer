cd0906bd0de1afaf0d2c52c71af82f72a4a28a32
Cloud User <ec2-user@ip-172-31-43-186.us-west-2.compute.internal>
2016-10-13 18:44:33 -0400

    taxhwfinished

--- taxes.sh
+++ taxes.sh
@@ -1,15 +1,20 @@
 function nextBracket {
-   echo "nextBracket"
+  fagi=$1
+  topOfBracket=$2
+  taxRate=$3
+  echo "your marginal tax rate is : $taxRate"
+  (( delta = $topOfBracket - $fagi ))  
+  echo "$delta more dollars untill the next bracket"  
 }
  
 read -p 'what is  your Federal Adjusted Gross Income? ' fagi 
 
-if [ $fagi -le 9275 ] ; then echo 10%; nextBracket #15percent $fagi  
-elif [ $fagi -ge 9276 ] && [ $fagi -le 37650 ] ; then echo 15%; nextBracket #15percent $fagi
-elif [ $fagi -ge 37651 ] && [ $fagi -le 91150 ] ; then echo 25%; nextBracket #25percent $fagi
-elif [ $fagi -ge 91151 ] && [ $fagi -le 190150 ] ; then echo 28%; nextBracket #28percent $fagi
-elif [ $fagi -ge 190151 ] && [ $fagi -le 413350 ] ; then echo 33%; nextBracket #33percent $fagi
-elif [ $fagi -ge 413351 ] && [ $fagi -le 415050 ] ; then echo 35%; nextBracket #35percent $fagi
-elif [ $fagi -ge 415051 ] ; then echo 39.6% 
+if [ $fagi -le 9275 ] ; then nextBracket $fagi 9275 10%  
+elif [ $fagi -ge 9276 ] && [ $fagi -le 37650 ] ; then nextBracket $fagi 37650 15%
+elif [ $fagi -ge 37651 ] && [ $fagi -le 91150 ] ; then nextBracket $fagi 91150 35%
+elif [ $fagi -ge 91151 ] && [ $fagi -le 190150 ] ; then nextBracket $fagi 190150 28%
+elif [ $fagi -ge 190151 ] && [ $fagi -le 413350 ] ; then nextBracket $fagi 413350 33%
+elif [ $fagi -ge 413351 ] && [ $fagi -le 415050 ] ; then nextBracket $fagi 415050 35%
+elif [ $fagi -ge 415051 ] ; then echo 39.6% ; echo "there is no higher bracket" 
 else echo " try again :) "
 fi  
