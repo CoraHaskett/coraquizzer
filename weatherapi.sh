df9c3d4ef75603a0baab33b2333f26bd0c3c886a
Cloud User <ec2-user@ip-172-31-43-186.us-west-2.compute.internal>
2016-10-20 19:07:35 -0400

    added read

--- weatherapi.sh
+++ weatherapi.sh
@@ -1,4 +1,5 @@
 read weatherapi_key <<< $(cat weatherapi_key)
+read date temp humidity <<< $(cat /tmp/weather.json | jq '.dt,.main.temp,.main.humidity')
 curl -s "http://api.openweathermap.org/data/2.5/weather?q=Minneapolis,us&units=imperial&APPID=$weatherapi_key"
 #while [ 1 == 1 ]
 #do 
