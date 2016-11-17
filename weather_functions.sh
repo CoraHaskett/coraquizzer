ca6fc9cf49427a8c73d1e6ac85c9bbd2fed4dc60
Cloud User <ec2-user@ip-172-31-43-186.us-west-2.compute.internal>
2016-11-17 09:21:15 -0500

    final

--- weather_functions.sh
+++ weather_functions.sh
@@ -1,27 +1,21 @@
 #the following will sum all the values of the temp_array and put them in a variable called 'sum'
-declare -a temp_array
 
+function average {
+   echo average= $(echo "scale=1; $sum / $samples" | bc)
+}
+
+declare -a temp_array
 for temp in $(cat ~/tmp/weather_data.csv | awk -F',' '{ print $2 }'); do
   temp_array=("${temp_array[@]}" $temp)
-
+done
 
 samples=$((${#temp_array[@]}-1))
+
 sum=0
 for temp in $(seq 0 $samples); do
   sum=$(echo "$sum + ${temp_array[$temp]}"|bc)
-
-
-echo $sum
-
-sum=$1
-samples=$((${#temp_array[@]}-1))
-
-function avg {
-   echo "avg temp"
-
-if [ 1=1 ] 
-then
-  avtp=$(($1 / $((${#temp_array[@]}-1))))
-  echo $avtp }
-  fi
 done
+
+echo "sum= $sum"
+echo "samples= $samples"
+average
