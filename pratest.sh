62519fddd4531c8fcfffdc4558f8c7578103e018
Cloud User <ec2-user@ip-172-31-43-186.us-west-2.compute.internal>
2016-12-13 15:07:04 -0500

    studying

--- pratest.sh
+++ pratest.sh
@@ -1,7 +1,8 @@
 #)For each year Barack Obama has been president, calculate the number of years since the year of your birth
 
-for i in {1..8}
-do echo years=$(($i + 7))
+for i in {2009..2016}
+do
+echo "Year of Presidency"=$i; echo "Years since I was born"=$(($i - 2001))
 
 done 
 
